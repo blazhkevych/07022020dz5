0d56c209ba1c9ba8107e19caa9523fc456af525e
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 19:37:56 +0300

    Добавьте файлы проекта.

---
+++ 07022020dz5.cpp
@@ -0,0 +1,23 @@
+﻿/*
+Задание 5: Пользователь вводит целое число А.Программа должна определить, что куб суммы цифр этого числа равен А* А
+*/
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int A, A1, sum = 0;
+	cout << "A = "; cin >> A;
+	if (A < 0) A = -A;
+	A1 = A;
+	while (A > 0)
+	{
+		sum += A % 10;
+		A /= 10;
+	}
+	if (sum * sum * sum == A1 * A1) cout << "Подходит\n";
+	else cout << "Не подходит\n";
+	return 0;
+}
